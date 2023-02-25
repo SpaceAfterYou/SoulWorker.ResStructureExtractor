2a5724226216e0f159dbc74cf8146f40a46c27e6
sawich <sawich@angeldev.org>
2023-02-25 15:02:05 +0200

    Path.Combine

--- SpaceAfterYou.SoulWorker.ResStructureExtractor.Test/TestExtract.cs
+++ SpaceAfterYou.SoulWorker.ResStructureExtractor.Test/TestExtract.cs
@@ -15,9 +15,9 @@ public sealed class TestExtract
     [TestMethod]
     public async Task FromUnpackedTest()
     {
-        var results = await Extract.FromUnpacked(@"Data\SoulWorker_dump.exe");
+        var results = await Extract.FromUnpacked(Path.Combine("Data", "SoulWorker_dump.exe"));
 
-        foreach (var file in Directory.EnumerateFiles(@"Data\Res"))
+        foreach (var file in Directory.EnumerateFiles(Path.Combine("Data", "Res")))
         {
             var name = Path.GetFileNameWithoutExtension(file);
             if (!results.Any(v => string.Equals(v.Key, name, StringComparison.OrdinalIgnoreCase)))
